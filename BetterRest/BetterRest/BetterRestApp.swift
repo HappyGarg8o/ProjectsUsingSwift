8b0b24e8066276f89ed60ef037d394f96a7b7d5b
HappyGarg8o <thehappygarg@icloud.com>
2025-03-16 10:36:09 +0530

    Add files via upload

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Aksh Garg on 10/03/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
