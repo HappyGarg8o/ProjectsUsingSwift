476c191aefa700d31953aaf8e4ef5f799d89acbc
HappyGarg8o <thehappygarg@icloud.com>
2025-04-07 10:44:10 +0530

    Add files via upload

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Aksh Garg on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
